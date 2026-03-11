69b6a077b9c5d68e6064e1ea2fffe73bf5ab4782
imsaurabh17 <saurabhdubey222786@gmail.com>
2026-03-11 19:00:58 +0530

    add a tool to calculate total investment

--- tools/stock_price.py
+++ tools/stock_price.py
@@ -1,9 +1,10 @@
 import os
 import requests
+from typing import Annotated
 import yfinance as yf
 from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage, AIMessage, ToolMessage
-from langchain_core.tools import tool
+from langchain_core.tools import tool, InjectedToolArg
 from langchain_groq import ChatGroq
 
 load_dotenv()
@@ -33,8 +34,17 @@ def get_exchange_rate(source_currency: str, target_currency: str) -> float:
         return conversion_rate
     else:
         return "API Failed to fetch the conversion rate"
+    
+@tool
+def calculate_investment(share: int, price: Annotated[float, InjectedToolArg], rate: Annotated[float,InjectedToolArg]) -> float:
+    """This tool will provide the total investment"""
+
+    investment = share * price * rate
+
+    return investment
 
 
 if __name__=="__main__":
-    print(get_stock_price.invoke({"stock_symbol":"AAPL"}))
-    print(get_exchange_rate.invoke({"source_currency":"USD","target_currency":"INR"}))
+    stock_price = get_stock_price.invoke({"stock_symbol":"AAPL"})
+    rate = get_exchange_rate.invoke({"source_currency":"USD","target_currency":"INR"})
+    print(calculate_investment.invoke({"share":10,"price":stock_price,"rate":rate}))
