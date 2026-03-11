cd2effe436c4e817b96942e0a7a4b07f567862fc
imsaurabh17 <saurabhdubey222786@gmail.com>
2026-03-11 13:56:20 +0530

    add price converter tool

---
+++ tools/price_converter.py
@@ -0,0 +1,31 @@
+import os
+import json
+from langchain_core.tools import tool
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+EXCHANGE_RATE_API = os.getenv("EXCHANGE_RATE_API")
+
+@tool
+def get_convert_rate(source_currency: str, target_currency: str) -> int:
+    """
+    This function will take source currency and the target currency and provide the live conversion rate of it using exchange rate api key.
+    """
+    url = f"https://v6.exchangerate-api.com/v6/{EXCHANGE_RATE_API}/pair/{source_currency}/{target_currency}"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        conversion_rate = data['conversion_rate']
+        return conversion_rate
+    else:
+        return f"Issue"
+
+
+if __name__=="__main__":
+
+    print(get_convert_rate.invoke({"source_currency": "USD", "target_currency": "INR"}))
