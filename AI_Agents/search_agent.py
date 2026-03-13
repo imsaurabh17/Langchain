0e2361e4648d655de3e3ce0c3f7ceec652129bcb
imsaurabh17 <saurabhdubey222786@gmail.com>
2026-03-13 13:08:46 +0530

    add llm

--- AI_Agents/search_agent.py
+++ AI_Agents/search_agent.py
@@ -3,7 +3,13 @@ from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_community.tools import DuckDuckGoSearchResults
 
+load_dotenv()
+
+API_KEY = os.getenv("GROQ_API_KEY")
+
 search_tool = DuckDuckGoSearchResults()
+
+llm = ChatGroq(model="llama-3.3-70b-versatile")
 
 
 if __name__=="__main__":
