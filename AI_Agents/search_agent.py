a785a2d1ea01e7b0b990d9cea0cdf6d491d6d79b
imsaurabh17 <saurabhdubey222786@gmail.com>
2026-03-13 16:35:27 +0530

    add get weather tool for extracting the temperature of a provided city

--- AI_Agents/search_agent.py
+++ AI_Agents/search_agent.py
@@ -1,26 +1,58 @@
 import os
+import requests
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_community.tools import DuckDuckGoSearchRun
-from langgraph.prebuilt import create_react_agent
+from langchain.agents import create_agent
+from langchain.tools import tool
 
 load_dotenv()
 
 API_KEY = os.getenv("GROQ_API_KEY")
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+
+@tool
+def get_weather(city: str) -> float:
+
+    """This tool will provide the temperatur of a provided city"""
+
+    geo_url = f"http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={WEATHER_API_KEY}"
+
+    response = requests.get(url=geo_url)
+
+    if response.status_code == 200:
+        geo_data = response.json()
+    
+    geo_data = geo_data[0]
+    lat = geo_data['lat']
+    lon = geo_data['lon']
+
+    weather_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={WEATHER_API_KEY}&units=metric"
+
+    weather_response = requests.get(weather_url)
+
+    if weather_response.status_code == 200:
+        weather_data = weather_response.json()
+
+    temperature = weather_data['main']['temp']
+
+    return temperature
 
 search_tool = DuckDuckGoSearchRun()
 
 llm = ChatGroq(model="openai/gpt-oss-120b")
 
-agent = create_react_agent(
+agent = create_agent(
     model=llm,
     tools=[search_tool]
 )
 
 
 if __name__=="__main__":
-    result = agent.invoke({
-        "messages": [{"role": "user", "content": "What is the current temperature in Mumbai?"}]
-    })
+    # result = agent.invoke({
+    #     "messages": [{"role": "user", "content": "What is the current temperature in Mumbai?"}]
+    # })
 
-    print(result["messages"][-1].content)
+    # print(result["messages"][-1].content)
+
+    print(get_weather.invoke({"city":"mumbai"}))
