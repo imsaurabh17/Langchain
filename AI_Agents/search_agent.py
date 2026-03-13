3305e0fc47a464aa82d9a31143f67bfe5ada648b
imsaurabh17 <saurabhdubey222786@gmail.com>
2026-03-13 13:30:16 +0530

    add ai agent with llm and tools

--- AI_Agents/search_agent.py
+++ AI_Agents/search_agent.py
@@ -1,16 +1,26 @@
 import os
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
-from langchain_community.tools import DuckDuckGoSearchResults
+from langchain_community.tools import DuckDuckGoSearchRun
+from langgraph.prebuilt import create_react_agent
 
 load_dotenv()
 
 API_KEY = os.getenv("GROQ_API_KEY")
 
-search_tool = DuckDuckGoSearchResults()
+search_tool = DuckDuckGoSearchRun()
 
-llm = ChatGroq(model="llama-3.3-70b-versatile")
+llm = ChatGroq(model="openai/gpt-oss-120b")
+
+agent = create_react_agent(
+    model=llm,
+    tools=[search_tool]
+)
 
 
 if __name__=="__main__":
-    print(search_tool.invoke("current temperature in mumbai"))
+    result = agent.invoke({
+        "messages": [{"role": "user", "content": "What is the current temperature in Mumbai?"}]
+    })
+
+    print(result["messages"][-1].content)
